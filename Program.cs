3d7d0b9a402e9f28c0af3d3883389bb2adeb9bac
WilliamIV <william.green.iv@gmail.com>
2016-09-28 20:57:45 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListSlide34HW
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                // Create a list for true/false
+                //decide is true/false
+                //print out
+                List<bool> boolList = new List<bool>() { true, false, false, true, false };
+
+                // Go through each to decide true-false
+                foreach (bool element in boolList)
+                {
+                    if (element == true)
+                    {
+                        Console.WriteLine("Better bring an umbrella");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No rain today, enjoy the sunshine!");
+                    }
+                }
+            }
+        }
+    }
